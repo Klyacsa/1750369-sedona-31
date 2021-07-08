e57e628c29eec94cf80cf47710959209e9aee484
klyacsa <vold-pav@yandex.ru>
2021-07-09 01:47:27 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 const modalLink = document.querySelector(".modal-open-button");
 const modalPopup = document.querySelector(".modal");
 
-modalLink.addEventListener("click", function () {
+modalLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.add("modal-show");
 });
