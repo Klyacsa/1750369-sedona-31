5f6674fa0b6766f215c4b930d3d10952226609df
Pavel Voldaev <vold-pav@yandex.ru>
2021-07-15 23:12:45 +0300

    На вторую проверку

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,11 @@
 const modalLink = document.querySelector(".modal-open-button");
 const modalPopup = document.querySelector(".modal");
+const modalForm = modalPopup.querySelector(".modal-form");
+const formDate = modalPopup.querySelector(".form-date");
+const formNumber = modalPopup.querySelector(".form-number");
 
 modalLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.toggle("modal-show");
   modalPopup.classList.toggle("modal");
 });
-
